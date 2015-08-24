7e1049f143e71ef12b55f4969f998a8e3bdb2d62
Ignacio Palladino <ipalladino@gmail.com>
2015-08-23 21:28:37 -0700

    Added setting to let carrierwave work on heroku

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,9 +9,20 @@ CarrierWave.configure do |config|
     :host                   => ENV['S3_HOST']
   }
 
+
+  # For testing, upload files to local `tmp` folder.
+  if Rails.env.test? || Rails.env.cucumber?
+   config.storage = :file
+   config.enable_processing = false
+   config.root = "#{Rails.root}/tmp"
+  else
+   config.storage = :fog
+  end
+
   config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
 
   config.fog_directory  = ENV['S3_BUCKET_NAME']                     # required
-  config.fog_public     = false                                   # optional, defaults to true
+  #config.fog_public     = false                                   # optional, defaults to true
+  config.s3_access_policy = :public_read
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
